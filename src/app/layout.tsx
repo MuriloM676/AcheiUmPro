9cd4b2da0a1b728c96993c292b6a062a85b4e928
PedroFSampaio <pesamp47@gmail.com>
2025-10-28 13:23:51 -0300

    atualizar front-end, questão de busca

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Providers from './providers'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={inter.className}>
         <Providers>
+          <Navbar />
           {children}
           <ToastContainer position="top-right" />
         </Providers>
